d628aa53028022391f39398f23eb452d55c8d9e5
len0rd <tylerm15@gmail.com>
2018-11-06 13:37:35 -0700

    script to load config settings. started making a simple script to install everything needed on fresh machine

---
+++ server/dev_setup.sh
@@ -0,0 +1,29 @@
+# top-level script to setup everything needed for a dev environment
+
+# detect machine
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     machine=Linux;;
+    Darwin*)    machine=Mac;;
+    CYGWIN*)    machine=Cygwin;;
+    MINGW*)     machine=MinGw;;
+    *)          machine="UNKNOWN:${unameOut}"
+esac
+
+if [ ${machine} = "Mac" ]; then
+    echo "Running mac installer"
+elif [ ${machine} = "Linux" ]; then
+    echo "Running linux installer"
+    echo "I'm going to ask for your su password to install postgres..."
+    sudo apt-get update
+    sudo apt-get install postgresql postgresql-contrib
+else 
+    echo "Sorry, I dont know how to properly install the server on your type of machine"
+    exit 1
+fi
+
+# Python connector for postgres
+# this assumes that you're environment is currently set to the python with ros installed
+# (this may not be the case if you're using conda or venv)
+echo "Install postgres python connector"
+# pip install psycopg2-binary
