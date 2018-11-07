5b0793ffdec32fd88eab576bfc1020dfe2c33da4
len0rd <tylerm15@gmail.com>
2018-11-07 09:19:06 -0700

    ros ingester doesn't immediately break

--- server/src/ros_ingest.py
+++ server/src/ros_ingest.py
@@ -1,33 +1,32 @@
+#! /usr/bin/env python
+
 import rospy
 from dao import DAO
-# from inertial_sense.msg import GPS
+from inertial_sense.msg import GPS
 
 class RosIngester:
-    """
-    Subscribes to the given topics and saves their information in 
-    a postgresql database
-    """
 
     dao_ = None
 
     def __init__(self):
         print("Startup ros ingester...")
         self.dao_ = DAO()
-        # self.gps_subscriber = rospy.Subscriber('/gps', GPS, self.gpsCallback, queue_size=10)
+        self.gps_subscriber = rospy.Subscriber('/gps', GPS, self.gpsCallback, queue_size=10)
         # self.raw_subscriber = rospy.Subscriber('/other_camera/image_raw/compressed', compressedImage)
         print("Ingester is all setup!")
         # gps type == inertial_sense/GPS
         # image == sensor_msgs/CompressedImage
 
 
-    # def gpsCallback(self, msg):
-    #     print("heyyyy")
+    def gpsCallback(self, msg):
+	print("heyyyy")
 
 def main():
+    print("Heyyyy")
     # initialize the node
     rospy.init_node('imaging_ingester')
 
-    subscriber = RosIngeter()
+    subscriber = RosIngester()
     # spin
     try:
         rospy.spin()
