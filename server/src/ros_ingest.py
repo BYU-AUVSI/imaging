92cab9f264b30c9e2fac516cec2cc7b149fb4d44
len0rd <tylerm15@gmail.com>
2018-11-08 08:44:59 -0700

    gps using a model class for dao

--- server/src/ros_ingest.py
+++ server/src/ros_ingest.py
@@ -3,6 +3,7 @@
 import rospy
 from dao import DAO
 from inertial_sense.msg import GPS
+from gps_msg import GpsMsg
 
 class RosIngester:
 
@@ -11,7 +12,8 @@ class RosIngester:
     def __init__(self):
         print("Startup ros ingester...")
         self.dao_ = DAO()
-        self.gps_subscriber = rospy.Subscriber('/gps', GPS, self.gpsCallback, queue_size=10)
+        self.gps_subscriber_ = rospy.Subscriber('/gps', GPS, self.gpsCallback, queue_size=10)
+        self.gps_msg_ = GpsMsg()
         # self.raw_subscriber = rospy.Subscriber('/other_camera/image_raw/compressed', compressedImage)
         print("Ingester is all setup!")
         # gps type == inertial_sense/GPS
@@ -23,12 +25,17 @@ class RosIngester:
             print("No GPS fix, cant add to db")
             return
 	    
-        time = msg.header.stamp.to_sec()
-        lat  = msg.latitude
-        lon  = msg.longitude
-        alt  = msg.altitude 
+        self.gps_msg_.time = msg.header.stamp.to_sec()
+        self.gps_msg_.lat  = msg.latitude
+        self.gps_msg_.lon  = msg.longitude
+        self.gps_msg_.alt  = msg.altitude 
 
-        print("{} :: {} {} {}".format(time, lat, lon, alt))
+        print("{} :: {} {} {}".format(self.gps_msg_.time, self.gps_msg_.lat, self.gps_msg_.lon, self.gps_msg_.alt))
+
+        # call dao with gps msg
+
+    def stateCallback(self, msg):
+        print("state callback")
 
 def main():
     print("Heyyyy")
