c936c7e10effd97a1baf12d71aab54f06b00e477
len0rd <tylerm15@gmail.com>
2018-11-08 09:19:42 -0700

    working on img callback

--- server/src/ros_ingest.py
+++ server/src/ros_ingest.py
@@ -1,8 +1,13 @@
 #! /usr/bin/env python
 
 import rospy
+import cv2
+import numpy as np
+import os, sys # for img saving
 from dao import DAO
+# ROS messages:
 from inertial_sense.msg import GPS
+from sensor_msgs.msg import CompressedImage
 from gps_msg import GpsMsg
 
 class RosIngester:
@@ -14,13 +19,22 @@ class RosIngester:
         self.dao_ = DAO()
         self.gps_subscriber_ = rospy.Subscriber('/gps', GPS, self.gpsCallback, queue_size=10)
         self.gps_msg_ = GpsMsg()
-        # self.raw_subscriber = rospy.Subscriber('/other_camera/image_raw/compressed', compressedImage)
+        self.subscriber = rospy.Subscriber("/other_camera/image_raw/compressed", CompressedImage, self.imgCallback,  queue_size = 10)
+        
+        # TODO: Remove me and use config file path
+        self.pathTmp="/home/len0rd/bags/img/raw"
+        os.mkdir(self.pathTmp)
         print("Ingester is all setup!")
         # gps type == inertial_sense/GPS
         # image == sensor_msgs/CompressedImage
 
 
     def gpsCallback(self, msg):
+        """
+        Ros subscriber callback. Subscribes to the inertial_sense GPS msg.
+        Get the lla values from the GPS message and then pass them to the DAO so 
+        they can be inserted into the database
+        """
         if msg.fix_type == GPS.GPS_STATUS_FIX_TYPE_NO_FIX or msg.num_sat == 0:
             print("No GPS fix, cant add to db")
             return
@@ -36,6 +50,19 @@ class RosIngester:
 
     def stateCallback(self, msg):
         print("state callback")
+
+    def imgCallback(self, msg):
+        """
+        Ros subscriber callback. Subscribes to the cameras image topic. Saves
+        the image file, and passes the corresponding filename and TS to the DAO
+        so that it can be inserted into the database
+        """
+        #get raw img data:
+        rawData = np.fromstring(msg.data, np.uint8)
+        ts = msg.header.stamp.to_nsec()
+        print("img callback: {}".format(ts))
+        filename = ts + ".jpg"
+        cv2.imwrite(self.pathTmp + filename, rawData)
 
 def main():
     print("Heyyyy")
