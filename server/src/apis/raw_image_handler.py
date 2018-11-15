f3d27cb21810ccba6d3fa4c493a66f3487bd6772
len0rd <tylerm15@gmail.com>
2018-11-15 09:23:03 -0700

    start trying to reply with images

--- server/src/apis/raw_image_handler.py
+++ server/src/apis/raw_image_handler.py
@@ -1,4 +1,4 @@
-from flask import jsonify
+from flask import jsonify, send_file
 from flask_restplus import Namespace, Resource, fields
 
 DEFAULT_ID = -1 # default to this if no id is specified in the request
@@ -10,6 +10,6 @@ api  = Namespace('image/raw', description="All imaging related calls route throu
 class RawImageHandler(Resource):
     def get(self, id=DEFAULT_ID):
         if id == DEFAULT_ID:
-            return jsonify({"yousent": "nothin"})
+            return send_file(filename, as_attachment=True, attachment_filename=filename, mimetype='image/jpeg')
         else:
             return jsonify({'yousent': id})
