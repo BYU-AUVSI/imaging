bd2b741e98d69e0579d990f60bf4dc6b4791d053
len0rd <tylerm15@gmail.com>
2018-11-15 09:23:46 -0700

    change crop coordinates to points

--- server/setup/setup_database.sql
+++ server/setup/setup_database.sql
@@ -49,8 +49,8 @@ CREATE TABLE "public"."manual_cropped" (
   raw_id int,
   time_stamp timestamp NOT NULL,
   cropped_path text NOT NULL,
-  crop_coordinate_tl int NOT NULL,
-  crop_coordinate_br int NOT NULL,
+  crop_coordinate_tl point NOT NULL,
+  crop_coordinate_br point NOT NULL,
   tapped boolean NOT NULL default FALSE,
   PRIMARY KEY ("id")
 );
