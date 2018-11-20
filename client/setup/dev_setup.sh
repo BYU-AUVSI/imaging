b8dc6052789ae214f7bc605c269b72e73369dcc6
len0rd <tylerm15@gmail.com>
2018-11-19 19:20:32 -0700

    adding setup scripts so that its easy to start developing stuff on different machines

---
+++ client/setup/dev_setup.sh
@@ -0,0 +1,15 @@
+echo "======================================="
+echo "NOTE: For a development setup, it is HIGHLY recommended"
+echo "that you use an environment manager such as conda or virtualenv"
+echo "======================================="
+
+read -p "Install using conda? (Y/N): " confirm
+
+if [ $confirm = "Y" -o $confirm = "y" ]; then
+    conda install --yes --file pip-requirements.txt
+    while read requirement; do conda install --yes $requirement; done < pip-requirements.txt
+elif [ $confirm = "N" -o $confirm = "n" ]; then
+    pip install -r pip-requirements.txt
+else
+    echo "Sorry, Im a stupid script who doesn't understand anything beyond exactly 'Y' or 'N'"
+fi
