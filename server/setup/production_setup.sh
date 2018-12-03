5fb99f63be8890ecd5c5cb719b4adabd42a2e447
len0rd <tylerm15@gmail.com>
2018-12-03 12:12:54 -0700

    ros stuff can now start. decided to just use ubuntu16s default python3.5 With the production setup script, the run command passes your current ROS_MASTER_URI to the docker image

--- server/setup/production_setup.sh
+++ server/setup/production_setup.sh
@@ -4,4 +4,7 @@ REPO=${PWD}/../..
 docker build -t byuauvsi/imaging_server ${REPO} -f ${PWD}/Dockerfile
 # run the server interactive tty will give it control of the terminal you run it in
 # so that it can print output --publish forwards the containers port 5000 to your port 5000
-docker run --interactive --tty --publish 5000:5000 --name imaging-server byuauvsi/imaging_server
+docker run --interactive --tty \
+  -e ROS_MASTER_URI="$ROS_MASTER_URI" \
+  --publish 5000:5000 \
+  --name imaging-server byuauvsi/imaging_server
