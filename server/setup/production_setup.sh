0ad39a99b06fd60655416a994ab48b738b7ee42e
len0rd <tylerm15@gmail.com>
2018-12-02 22:44:27 -0700

    docker container server starts and is accessible on local network. Added arg parse to server.py so you can specify host

--- server/setup/production_setup.sh
+++ server/setup/production_setup.sh
@@ -1,6 +1,7 @@
-SERVER=${PWD}/..
-REPO=${SERVER}/..
+
+REPO=${PWD}/../..
 # docker build  tagname path pathToDockerFile
 docker build -t byuauvsi/imaging_server ${REPO} -f ${PWD}/Dockerfile
-#- -publish 5000:5000
-docker run --interactive --tty --name imaging-server byuauvsi/imaging_server
+# run the server interactive tty will give it control of the terminal you run it in
+# so that it can print output --publish forwards the containers port 5000 to your port 5000
+docker run --interactive --tty --publish 5000:5000 --name imaging-server byuauvsi/imaging_server
