c9ffeb4ad4b9aa327431b519c89f61d4d47af1e3
len0rd <tylerm15@gmail.com>
2018-12-06 16:06:23 -0700

    added reset method for autonomous tables

--- server/src/apis/util_handler.py
+++ server/src/apis/util_handler.py
@@ -3,18 +3,35 @@ from dao.util_dao import UtilDAO
 from flask_restplus import Namespace, Resource
 
 
-api = Namespace('util', description="Some utility methods for debugging")
+api = Namespace('util', description="Some utility methods for development/debugging")
 
 @api.route('/reset/manual')
-class UtilResetHandler(Resource):
+class UtilManualResetHandler(Resource):
     @api.doc(description="""
         WARNING! README!! This resets ALL images. This will remove all 
         cropped and classified images and reset any manual tapped images in
         incoming to False. Essentially this leaves you with a clean database
-        as if a rosbag was just read in.""")
+        as if the rostopics were just read in (meaning images aren't entirely 
+        erased and the incoming_image table is not truncated)""")
     @api.doc(responses={200:'OK', 500:'Reset failed'})
     def get(self):
         dao = UtilDAO(defaultConfigPath())
-        dao.resetDB()
+        dao.resetManualDB()
         
         return {'message': 'Success!'}
+
+@api.route('/reset/autonomous')
+class UtilAutoResetHandler(Resource):
+    @api.doc(description="""
+        WARNING! README!! This resets ALL images. This will remove all 
+        autonomous classified images and reset any autonomous tapped images in
+        incoming to False. Essentially this leaves you with a clean database
+        as if the rostopics were just read in (meaning images aren't entirely 
+        erased and the incoming_image table is not truncated)""")
+    @api.doc(responses={200:'OK', 500:'Reset failed'})
+    def get(self):
+        dao = UtilDAO(defaultConfigPath())
+        dao.resetAutonomousDB()
+
+        return {'message': 'Success!'}
+
--- server/src/dao/util_dao.py
+++ server/src/dao/util_dao.py
@@ -5,14 +5,23 @@ class UtilDAO(BaseDAO):
     def __init__(self, configFilePath):
         super(UtilDAO, self).__init__(configFilePath)
 
-    def resetDB(self):
+    def resetManualDB(self):
         """
-        Resets the database to an inital form as if a rosbag
+        Resets the database to an initial form as if a rosbag
         was just read in
         """
-
         truncateCropped = "DELETE FROM manual_cropped;"
         truncateClassification = "DELETE FROM outgoing_manual;"
         updateIncoming = "UPDATE incoming_image SET manual_tap=FALSE WHERE manual_tap=TRUE;"
 
         super(UtilDAO, self).executeStatements([truncateClassification, truncateCropped, updateIncoming])
+
+    def resetAutonomousDB(self):
+        """
+        Resets the database to an initial form as if a rosbag
+        was just read in
+        """
+        truncateClassification = "DELETE FROM outgoing_autonomous;"
+        updateIncoming = "UPDATE incoming_image SET autonomous_tap=FALSE WHERE autonomous_tap=TRUE;"
+
+        super(UtilDAO, self).executeStatements([truncateClassification, updateIncoming])
