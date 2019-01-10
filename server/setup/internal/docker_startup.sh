4baa40fe2fa8f2fac5d302fc103475cfc34bf5e9
len0rd <tylerm15@gmail.com>
2019-01-09 19:07:39 -0700

    seperate docker stuff into setup and start files. add comments on nuances of running in non-linux env

--- server/setup/internal/docker_startup.sh
+++ server/setup/internal/docker_startup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-service postgresql start
-source /opt/catkin_ws/devel/setup.sh
-# export ROS_HOSTNAME=`hostname -I`
-/usr/bin/supervisord
-# rosrun imaging_ros_ingester ros_ingest.py
+# this script runs on the docker image and tells it what todo when it starts up
+service postgresql start # start postgres
+source /opt/catkin_ws/devel/setup.sh # source our dev workspace
+export ROS_HOSTNAME=`hostname -I`
+/usr/bin/supervisord # start supervisor, which starts the server and ros_ingestor in the background
---
+++ server/setup/setup_docker_image.sh
@@ -0,0 +1,31 @@
+echo "========================"
+echo "Hi there. I'm going to build the docker image for you."
+echo "========================"
+REPO=${PWD}/../..
+# docker build  tagname path pathToDockerFile
+docker build -t byuauvsi/imaging_server ${REPO} -f ${PWD}/Dockerfile
+
+
+echo "DONE! with the docker image"
+
+# detect machine
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     machine=Linux;;
+    Darwin*)    machine=Mac;;
+    CYGWIN*)    machine=Cygwin;;
+    MINGW*)     machine=MinGw;;
+    *)          machine="UNKNOWN:${unameOut}"
+esac
+
+if [ ${machine} = "Linux" ]; then
+    echo "Looks like you're running linux. Neat!"
+    echo "I could edit your crontab so that this docker image"
+    echo "starts automatically when the machine boots."
+    echo ""
+    read -p "Edit crontab? (Y/N)" confirm
+    if [ $confirm = "Y" -o $confirm = "y" ]; then
+        # copy command into crontab and set it to run @reboot
+        echo "@reboot docker run --network host --interactive --tty -e ROS_MASTER_URI=\"$ROS_MASTER_URI\" --publish 5000:5000 --name imaging-server byuauvsi/imaging_server" | sudo tee -a /var/spool/cron/crontabs/$USER
+    fi
+fi
