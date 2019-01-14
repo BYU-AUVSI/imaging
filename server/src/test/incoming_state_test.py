5ed3c95dff1f91a68088a6669733cf5cc8a9e795
len0rd <tylerm15@gmail.com>
2019-01-14 11:02:27 -0700

    incoming state and gps done

--- server/src/test/incoming_state_test.py
+++ server/src/test/incoming_state_test.py
@@ -1,95 +1,96 @@
 import unittest
 from config import defaultConfigPath
 from test.test_helpers import truncateTable
-from dao.incoming_gps_dao import IncomingGpsDAO
-from dao.model.incoming_gps import incoming_gps
+from dao.incoming_state_dao import IncomingStateDAO
+from dao.model.incoming_state import incoming_state
 
 
-class TestGpsInsert(unittest.TestCase):
+class TestStateInsert(unittest.TestCase):
     def test(self):
-        model = incoming_gps()
+        model = incoming_state()
         model.time_stamp = 1547453775.2
-        model.lat = 40.111
-        model.lon = -111.222
-        model.alt = 1234.5
+        model.roll = 40.111
+        model.pitch = -111.222
+        model.yaw = 12.3
 
-        truncateTable('incoming_gps')
-        dao = IncomingGpsDAO(defaultConfigPath())
+        truncateTable('incoming_state')
+        dao = IncomingStateDAO(defaultConfigPath())
         self.assertIsNotNone(dao)
-        resultingId = dao.addGps(model)
+        resultingId = dao.addState(model)
         self.assertIsNotNone(resultingId)
         self.assertNotEqual(resultingId, -1)
 
-class TestGpsGetById(unittest.TestCase):
+class TestStateGetById(unittest.TestCase):
     def test(self):
-        model = incoming_gps()
+        model = incoming_state()
         model.time_stamp = 1547453775.2
-        model.lat = 40.111
-        model.lon = -111.222
-        model.alt = 1234.5
+        model.roll = 40.111
+        model.pitch = -111.222
+        model.yaw = 12.3
 
-        truncateTable('incoming_gps')
-        dao = IncomingGpsDAO(defaultConfigPath())
+        truncateTable('incoming_state')
+        dao = IncomingStateDAO(defaultConfigPath())
         self.assertIsNotNone(dao)
-        resultingId = dao.addGps(model)
+        resultingId = dao.addState(model)
         self.assertNotEqual(resultingId, -1)
 
-        gottenMeas = dao.getGpsById(resultingId)
+        gottenMeas = dao.getStateById(resultingId)
         self.assertIsNotNone(gottenMeas)
 
         self.assertEqual(gottenMeas.id, resultingId)
         self.assertAlmostEqual(gottenMeas.time_stamp, 1547453775.2)
-        self.assertAlmostEqual(gottenMeas.lat, 40.111)
-        self.assertAlmostEqual(gottenMeas.lon, -111.222)
-        self.assertAlmostEqual(gottenMeas.alt, 1234.5)
+        self.assertAlmostEqual(gottenMeas.roll, 40.111)
+        self.assertAlmostEqual(gottenMeas.pitch, -111.222)
+        self.assertAlmostEqual(gottenMeas.yaw, 12.3)
 
-class TestGpsGetByTs(unittest.TestCase):
+class TestStateGetByTs(unittest.TestCase):
     def test(self):
         # Note: the closest TS function doesnt work by absolute closest,
         # but instead for ease, speed and readability, just checks <= 
         # These tests reflect this type of functionality and would probably have
         # to be redone if true closest TS was ever implemented
 
-        truncateTable('incoming_gps')
-        dao = IncomingGpsDAO(defaultConfigPath())
+        truncateTable('incoming_state')
+        dao = IncomingStateDAO(defaultConfigPath())
         self.assertIsNotNone(dao)
 
         baseTs = 1547453775.2
         # test on empty table
-        resultModel = dao.getGpsByClosestTS(baseTs)
+        resultModel = dao.getStateByClosestTS(baseTs)
         self.assertIsNone(resultModel)
         
-        model = incoming_gps()
+        model = incoming_state()
         model.time_stamp = baseTs
-        model.lat = 40.111
-        model.lon = -111.222
-        model.alt = 1234.5
-        resultingId1 = dao.addGps(model)
+        model.roll = 40.111
+        model.pitch = -111.222
+        model.yaw = 12.3
+        resultingId1 = dao.addState(model)
         self.assertNotEqual(resultingId1, -1)
 
         model.time_stamp = baseTs + 20000
-        model.lat = 40.222
-        model.lon = -111.333
-        model.alt = 5678.9
-        resultingId2 = dao.addGps(model)
+        model.roll = 40.222
+        model.pitch = -111.333
+        model.yaw = 567.8
+        resultingId2 = dao.addState(model)
         self.assertNotEqual(resultingId2, -1)
 
         # as explained above, this should return None
-        resultModel = dao.getGpsByClosestTS(baseTs - 10000)
+        resultModel = dao.getStateByClosestTS(baseTs - 10000)
         self.assertIsNone(resultModel)
 
-        resultModel = dao.getGpsByClosestTS(baseTs + 1000)
+        resultModel = dao.getStateByClosestTS(baseTs + 1000)
         self.assertIsNotNone(resultModel)
         self.assertEqual(resultModel.id, resultingId1)
 
         # while this is absolutely closer to id2, we should
         # still get id1 for the spec reasons described at the 
         # top of this method
-        resultModel = dao.getGpsByClosestTS(baseTs + 15000)
+        resultModel = dao.getStateByClosestTS(baseTs + 15000)
         self.assertIsNotNone(resultModel)
         self.assertEqual(resultModel.id, resultingId1)
 
         # test when time is exactly equal
-        resultModel = dao.getGpsByClosestTS(baseTs + 20000)
+        resultModel = dao.getStateByClosestTS(baseTs + 20000)
         self.assertIsNotNone(resultModel)
         self.assertEqual(resultModel.id, resultingId2)
+
