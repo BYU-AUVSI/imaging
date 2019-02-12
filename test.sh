33256578898a377a5ef17fbc2f12203beb8d6723
len0rd <tylerm15@gmail.com>
2019-02-12 08:22:39 -0700

    nicer test result printing

--- test.sh
+++ test.sh
@@ -1,28 +1,35 @@
 #!/bin/bash
 
-function verifyLastExit() {
-    retVal=$? # return value of last command
-    if [ $retVal -ne 0 ]; then
-        # last command had a non-zero exit, we're done
-        exit $retVal
-    fi
+function echo_red    { echo -e "\033[1;31m$@\033[0m"; }
+function echo_green  { echo -e "\033[1;32m$@\033[0m"; }
+function echo_blue   { echo -e "\033[1;34m$@\033[0m"; }
+
+function print_result() {
+  if [ $1 -eq 0 ]; then
+    echo_green "[Passed]"
+  else
+    # last command had a non-zero exit, we're done
+    echo_red "[Failed]"
+    exit $1
+  fi
+  echo ""
 }
 
 # run the dao unit tests first
+echo_blue "Test 1: Server DAO Unit Tests"
 cd server/src
 python test.py
-verifyLastExit
+print_result $? # check return value of last command
 
-echo "=============================="
+
 echo "Setup db for client_rest tests"
 python setup_db_for_client_tests.py
-verifyLastExit
-
 python server.py &
 serverPID=$! # get PID for last executed command
 sleep 5
 
 # move to the client_rest test directory
+echo_blue "Test 2: Client Rest Integration Tests"
 cd ../../client/lib
 python test.py
 retVal=$? # return value of last command
@@ -35,7 +42,4 @@ let otherServerPID=$serverPID+2
 # the || makes it fail quietly if there's no second PID
 kill -9 $otherServerPID || echo "No second PID to kill"
 
-if [ $retVal -ne 0 ]; then
-    # last command had a non-zero exit, we're done
-    exit $retVal
-fi
+print_result $retVal
