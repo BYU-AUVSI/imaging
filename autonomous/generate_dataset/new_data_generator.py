34d909958bbcf0309399da6f01ec41c46ffb0389
len0rd <tylerm15@gmail.com>
2019-02-22 10:30:13 -0700

    shapes are generated in a folder of their name. checks to make sure the shape isnt on the border of the image before saving

--- autonomous/generate_dataset/new_data_generator.py
+++ autonomous/generate_dataset/new_data_generator.py
@@ -1,4 +1,5 @@
 import cv2 as cv
+import os
 import numpy as np
 from sklearn.cluster import MiniBatchKMeans
 from matplotlib import pyplot as plt
@@ -7,8 +8,14 @@ from scipy.spatial import distance as dist
 import imutils
 import time
 
+name = "cross"
+save_dir = 'shapes/' + name + '/'
+
+currentPath = os.path.dirname(os.path.realpath(__file__))
+if not os.path.exists(currentPath + '/' + save_dir):
+    os.makedirs(currentPath + '/' + save_dir)
 #Import the video
-cap = cv.VideoCapture('test.mov')
+cap = cv.VideoCapture(name + '.mov')
 
 #Uncomment these to save a video
 #fourcc = cv.VideoWriter_fourcc(*'XVID')
@@ -19,12 +26,21 @@ write_count = 0
 count = 0
 
 #Loop throuogh the total number of video frames
-while count < 300 and cap.isOpened():
+while cap.isOpened(): # and count < 300 <-- if you want to limit the amount of runtime
     #Read a new frame
     ret, frame = cap.read()
+    
+    if not ret:
+        print("no frame!!")
+        break
+
     #Resize, crop, and resize again to obtain 200x200 image with target in center
-    frame = imutils.resize(frame, width = 600)
-    frame_crop = frame[400:950, 50:600]
+    # probably need to adjust this a bit to get the  target properly in frame
+    frame_crop = imutils.resize(frame, width = 750)
+    # print(frame_crop.shape)
+    frame_crop = frame_crop[20:420, 200:600]
+    # print(frame_crop.shape)
+    # break
     frame_crop = imutils.resize(frame_crop, width = 200)
 
     #Blur and posterize. Can change parameters for random blur if desired
@@ -60,9 +76,20 @@ while count < 300 and cap.isOpened():
             cv.drawContours(flood, [c], 0, 255, cv.FILLED)
             #erode the mask to eliminate any leftover background
             flood = cv.erode(flood, None, iterations=2)
-            #Write the mask to a file
-            cv.imwrite('data/trap%i.jpg' % (write_count), flood)
-            write_count += 1
+
+            # sum along rows and columns, really we're only interested in the 
+            # first and last row and column (ie: a box around the image of the last pixels)
+            rowSum = np.sum(flood, axis=1)
+            colSum = np.sum(flood, axis=0)
+            maxDim = len(rowSum) - 1 # should be square
+
+            # dont write if the shape its against the edge of the image
+            #   shapes absorbed by the edge wont happen irl, and it screws with 
+            #   the target shape, such that we shouldnt be able to id it anyways
+            if rowSum[0] < 1 and rowSum[maxDim] < 1 and colSum[0] < 1 and colSum[maxDim] < 1:  
+                #Write the mask to a file
+                cv.imwrite(save_dir + name + '%i.jpg' % (write_count), flood)
+                write_count += 1
 
     #Show crop and mask
     cv.imshow('frame',frame_crop)
