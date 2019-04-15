8175cd62c15652ad150566d105653d24a2f276a4
Jake Johnson <jacobcjohnson13@gmail.com>
2019-04-15 08:53:08 -0600

    Added updated autonomous files

--- autonomous/src/autonomous_classification.py
+++ autonomous/src/autonomous_classification.py
@@ -8,21 +8,26 @@ import imutils
 import time
 from autonomous_detection import AutonomousDetection
 
+#TODO: Methods for rejecting false positives to implement:
+#       Compare how much of edge of "letter" matches with edge of
+#           shape. If too high, reject
+#       Compare ratio of size of letter to size of shape. If too low, reject
+
 class AutonomousClassification():
 
     def __init__(self):
-        #create a library for color determination
+        #create a library for color determination in RGB
         colors = OrderedDict({
-        	"red": (67,40,31),#255,0,0
-        	"green": (24,82,51),#0,255,0
-        	"blue": (25,36,63),#0,0,255
-            "white": (129,142,154),#255,255,255
+        	"red": (215,150,150),#255,0,0
+        	"green": (100,255,200),#0,255,0
+        	"blue": (80,200,200),#0,0,255
+            "white": (255,255,255),#255,255,255
             "black": (0,0,0),
-            "orange": (255,140,0),
-            "yellow": (255,255,0),
-            "purple": (128,0,128),
-            "gray": (128,128,128),
-            "brown": (139,69,19)})
+            "orange": (250,200,100),
+            "yellow": (250,250,190),
+            "purple": (170,165,250),
+            "gray": (200,200,200),
+            "brown": (220,210,220)})
 
         # allocate memory for the L*a*b* image, then initialize
         # the color names list
@@ -37,69 +42,52 @@ class AutonomousClassification():
         # convert the L*a*b* array from the RGB color space
         # to L*a*b*
         self.lab_dict = cv.cvtColor(self.lab_dict, cv.COLOR_RGB2LAB)
-        print(self.lab_dict)
-        print(self.colorNames)
 
         self.img_crop = []
         self.blur_crop = []
         self.canny_crop = []
         self.flood_crop = []
+        self.c = []
         self.big_contour = []
         self.white_back = []
         self.cluster = []
         self.centers = []
 
 
-    def load_new_image(self, img, resize_shape, keypoints):
+    def load_new_crops(self, img_crop):
 
-        self.original_image = img
-        self.x_orig, self.y_orig, _ = img.shape
-        self.x_resize, self.y_resize, _ = resize_shape
-        self.keypoints = keypoints
         self.clear_lists()
+        for i in range(len(img_crop)):
+            self.img_crop.append(imutils.resize(img_crop[i], width=200))
 
-
-    def crop_ROIs(self, show=False):
-
-        for i in range(0,len(self.keypoints)):
-
-            #find the keypoint in the resized picture
-            x = int(self.keypoints[i].pt[0])
-            y = int(self.keypoints[i].pt[1])
-
-            #find the keypoint in the original picture
-            x_new = int(self.x_orig*x/self.x_resize)
-            y_new = int(self.y_orig*y/self.y_resize)
-
-            #crop the potential target from the orignal image
-            crop = img[y_new-30:y_new+40, x_new-30:x_new+40]
-            self.img_crop.append(crop)
-
-            if show:
-                cv.imshow('Cropped Image %i' % (i), self.img_crop[i])
 
     def get_mask(self, show=False):
 
-        for i in range(0, len(self.img_crop)):
+        for i in range(len(self.img_crop)):
 
-            self.blur_crop.append(cv.GaussianBlur(self.img_crop[i], (5, 5), 0))
+            self.blur_crop.append(cv.GaussianBlur(self.img_crop[i], (5, 5), 0))     #**IS THIS NECESSARY?**
             self.blur_crop[i] = cv.pyrMeanShiftFiltering(self.blur_crop[i], 30, 30, 3)
-            #cv.imshow('Blur %i' % (i), blur_crop[i])
+            cv.imshow('Blur %i' % (i), self.blur_crop[i])
 
             #detect edges and show
             self.canny_crop.append(cv.Canny(self.blur_crop[i],10,200))
-            # canny_crop[i] = cv.dilate(canny_crop[i], None, iterations=2)
-            # canny_crop[i] = cv.erode(canny_crop[i], None, iterations=2)
-            #cv.imshow('Canny %i' % (i), canny_crop[i])
+            #dilating the edges often closes edges that were originally not connected
+            self.canny_crop[i] = cv.dilate(self.canny_crop[i], None, iterations=1)
+            #self.canny_crop[i] = cv.erode(self.canny_crop[i], None, iterations=1)
+            cv.imshow('Canny %i' % (i), self.canny_crop[i])
 
             #fill the enclosed edges to create a mask and show
             h, w = self.canny_crop[i].shape[:2]
+            self.canny_crop[i][0,:] = 0
+            self.canny_crop[i][h-1,:] = 0
+            self.canny_crop[i][:,0] = 0
+            self.canny_crop[i][:,w-1] = 0
             mask = np.zeros((h+2, w+2), np.uint8)
             edges = self.canny_crop[i].copy()
             cv.floodFill(edges, mask, (0,0), 255)
             edges = cv.bitwise_not(edges)
             self.flood_crop.append(self.canny_crop[i] | edges)
-            #cv.imshow('Flood %i' % (i), self.flood_crop[i])
+            self.flood_crop[i] = cv.erode(self.flood_crop[i], None, iterations=2)
 
             #find the contours in the filled mask
             cnts = cv.findContours(self.flood_crop[i].copy(), cv.RETR_EXTERNAL,
@@ -109,91 +97,172 @@ class AutonomousClassification():
             #if there is at least one countour,
             if len(cnts) > 0:
                 #find the biggest contour
-                c = max(cnts, key=cv.contourArea)
+                self.c.append(max(cnts, key=cv.contourArea))
                 #remake the mask with only the biggest contour and show
                 self.flood_crop[i][:,:] = 0
-                cv.drawContours(self.flood_crop[i], [c], 0, 255, cv.FILLED)
+
+                cv.drawContours(self.flood_crop[i], [self.c[i]], 0, 255, cv.FILLED)
+                #erode the mask to eliminate any leftover background
+                #self.flood_crop[i] = cv.erode(self.flood_crop[i], None, iterations=2)
+
+                # M = cv.moments(self.c[i])
+                # cX = int(M["m10"] / M["m00"])
+                # cY = int(M["m01"] / M["m00"])
+                # self.blur_crop[i] = self.blur_crop[i][max(1,cY-70):cY+70, max(1,cX-70):cX+70]
+                # self.flood_crop[i] = self.flood_crop[i][max(1,cY-70):cY+70, max(1,cX-70):cX+70]
 
                 if show:
                     cv.imshow('Flood %i' % (i), self.flood_crop[i])
 
+            else:
+                #print('Flood %i failed: no significant contours in crop' % (i))
+                self.flood_crop[i] = None
+                self.c.append(None)
+
     def get_white_back(self, show=False):
-
+        #NOTE: Changing color to black
         for i in range(0, len(self.flood_crop)):
+            if self.flood_crop[i] is not None:
+                self.white_back.append(cv.bitwise_and(self.blur_crop[i],self.blur_crop[i],mask=self.flood_crop[i]))
+                #self.white_back[i][np.where((self.white_back[i]==[0,0,0]).all(axis=2))] = [255,255,255]
 
-            self.white_back.append(cv.bitwise_and(self.blur_crop[i],self.blur_crop[i],mask=self.flood_crop[i]))
-            self.white_back[i][np.where((self.white_back[i]==[0,0,0]).all(axis=2))] = [255,255,255]
+                if show:
+                    cv.imshow('White Background %i' % (i), self.white_back[i])
 
-            if show:
-                cv.imshow('White Background %i' % (i), self.white_back[i])
+            else:
+                self.white_back.append(None)
 
     def color_cluster(self, show=False):
 
         for i in range(0,len(self.white_back)):
+            if self.white_back[i] is not None:
+                #convert to lab for color identification
+                self.cluster.append(cv.cvtColor(self.white_back[i], cv.COLOR_BGR2LAB))
+                h,w = self.cluster[i].shape[:2]
+                self.cluster[i] = self.cluster[i].reshape((self.cluster[i].shape[0] * self.cluster[i].shape[1], 3))
 
-            #convert to lab for color identification
-            self.cluster.append(cv.cvtColor(self.white_back[i], cv.COLOR_BGR2LAB))
-            h,w = self.cluster[i].shape[:2]
-            self.cluster[i] = self.cluster[i].reshape((self.cluster[i].shape[0] * self.cluster[i].shape[1], 3))
+                #reduce to 3 colors (one will be the white background)
+                clt = MiniBatchKMeans(n_clusters = 3, reassignment_ratio=0.2)
+                labels = clt.fit_predict(self.cluster[i])
 
-            #reduce to 3 colors (one will be the white background)
-            clt = MiniBatchKMeans(n_clusters = 3)
-            labels = clt.fit_predict(self.cluster[i])
+                self.cluster[i] = clt.cluster_centers_.astype("uint8")[labels]
 
-            print(clt.cluster_centers_)
+                #print(clt.cluster_centers_)
+                #eliminate white from the array of colors so only two remain
+                #NOTE: now looks for black
+                for j in range(3):
+                    if clt.cluster_centers_[j,0] < 2.0 and 127.0 < clt.cluster_centers_[j,1] < 129.0 and 127.0 < clt.cluster_centers_[j,2] < 129.0:
+                        self.centers = np.delete(clt.cluster_centers_, j, 0)
+                        break
 
-            self.cluster[i] = clt.cluster_centers_.astype("uint8")[labels]
+                #reshape image
+                self.cluster[i] = self.cluster[i].reshape((h, w, 3))
 
-            #eliminate white from the array of colors so only two remain
-            for j in range(3):
-                if clt.cluster_centers_[j,0] > 254.5 and clt.cluster_centers_[j,1] > 127.5 and clt.cluster_centers_[j,2] > 127.5:
-                    self.centers = np.delete(clt.cluster_centers_, j, 0)
+                #create masks to determine how many pixels are each color. The higher number is the target color and the lower is the letter color
+                c1 = cv.inRange(self.cluster[i], self.centers[0,:]-1, self.centers[0,:]+1)
+                c2 = cv.inRange(self.cluster[i], self.centers[1,:]-1, self.centers[1,:]+1)
+                count1 = cv.countNonZero(c1)
+                count2 = cv.countNonZero(c2)
 
-            #reshape image
-            self.cluster[i] = self.cluster[i].reshape((h, w, 3))
+                if count1 < count2:
+                    temp = np.copy(self.centers[0,:])
+                    self.centers[0,:] = self.centers[1,:]
+                    self.centers[1,:] = temp
 
-            #create masks to determine how many pixels are each color. The higher number is the target color and the lower is the letter color
-            c1 = cv.inRange(self.cluster[i], self.centers[0,:]-1, self.centers[0,:]+1)
-            c2 = cv.inRange(self.cluster[i], self.centers[1,:]-1, self.centers[1,:]+1)
-            count1 = cv.countNonZero(c1)
-            count2 = cv.countNonZero(c2)
-            print(count1)
-            print(count2)
+                #print(self.centers)
+                #Reject if one of the colors is black
+                for j in range(2):
+                    if self.centers[j,0] < 2.0 and 127.0 < self.centers[j,1] < 129.0 and 127.0 < self.centers[j,2] < 129.0:
+                        #print('Cluster %i failed: Not enough colors identified' % (i))
+                        self.cluster[i] = None
+                        break
 
-            if count1 < count2:
-                temp = np.copy(self.centers[0,:])
-                self.centers[0,:] = self.centers[1,:]
-                self.centers[1,:] = temp
+                d_centers = dist.euclidean(self.centers[0], self.centers[1])
+                if d_centers < 25:#15
+                    self.cluster[i] = None
+                    #print('Cluster %i failed: Clustered too close' % (i))
+                    continue
 
-            print(self.centers)
+                #NOTE: now changes letter to white and background to black
+                if self.cluster[i] is not None:
+                    self.cluster[i][np.where((self.cluster[i].astype(int)==self.centers[0,:].astype(int)).all(axis=2))] = [0,128,128]
+                    self.cluster[i][np.where((self.cluster[i].astype(int)==self.centers[1,:].astype(int)).all(axis=2))] = [255,128,128]
+                    self.cluster[i] = cv.cvtColor(self.cluster[i], cv.COLOR_LAB2BGR)
+
+                    self.cluster[i] = cv.cvtColor(self.cluster[i], cv.COLOR_BGR2GRAY)
+                    cnts = cv.findContours(self.cluster[i].copy(), cv.RETR_CCOMP,
+                        cv.CHAIN_APPROX_SIMPLE)
+                    contours = cnts[1]
+                    hierarchy = cnts[2]
+
+                    #if there is at least one countour,
+                    if len(contours) > 0:
+                        #find the biggest contour
+                        areas = [cv.contourArea(c) for c in contours] # get the area of each contour
+                        max_index = np.argmax(areas)
+                        c = max(contours, key=cv.contourArea)
+                        #remake the mask with only the biggest contour and show
+                        self.cluster[i][:,:] = 0
+
+                        cv.drawContours(self.cluster[i], [c], 0, 255, cv.FILLED)
+                        child = hierarchy[0][max_index][2]
+                        if child != -1:
+                            cv.drawContours(self.cluster[i], [contours[child]], 0, 0, cv.FILLED)
+
+                        self.cluster[i] = cv.bitwise_not(self.cluster[i])
+
+                        #Two false positive checks:
+                        #   If a large portion of the edge of the "letter" is on the border
+                        #   If the "letter" isn't big enough related to the shape
+                        letter_edge = cv.Canny(self.cluster[i], 10, 50)
+                        letter_edge_count = cv.countNonZero(letter_edge)
+                        shape_edge = cv.Canny(self.flood_crop[i], 10, 50)
+                        combined_edge = cv.bitwise_and(letter_edge, shape_edge)
+                        combined_edge_count = cv.countNonZero(combined_edge)
+
+                        h,w = self.cluster[i].shape
+                        letter_area = h*w - cv.countNonZero(self.cluster[i])
+                        shape_area = cv.countNonZero(self.flood_crop[i])
+
+                        if combined_edge_count/letter_edge_count > 0.1 or letter_area/shape_area < 0.05:
+                            #print("Cluster %i failed: Letter not likely" % (i))
+                            self.cluster[i] = None
 
 
-            #self.cluster[i][np.where((self.cluster[i].astype(int)==self.centers[0,:].astype(int)).all(axis=2))] = [0,128,128]
-            #self.cluster[i][np.where((self.cluster[i].astype(int)==self.centers[1,:].astype(int)).all(axis=2))] = [255,128,128]
-            self.cluster[i] = cv.cvtColor(self.cluster[i], cv.COLOR_LAB2BGR)
+                    if show and self.cluster[i] is not None:
+                        cv.imshow('Quantized %i' % (i), self.cluster[i])
 
-            if show:
-                cv.imshow('Quantized %i' % (i), self.cluster[i])
+                    #Deterimine colors
+                    for count in range(2):
+                        minDist = (np.inf, None)
+                        for (k, row) in enumerate(self.lab_dict):
+    			            # compute the distance between the current L*a*b*
+    			            # color value and the mean of the image
+                            d = dist.euclidean(row[0], self.centers[count])
 
-            for count in range(2):
-                minDist = (np.inf, None)
-                for (k, row) in enumerate(self.lab_dict):
-    			    # compute the distance between the current L*a*b*
-    			    # color value and the mean of the image
-                    d = dist.euclidean(row[0], self.centers[count])
+    			            # if the distance is smaller than the current distance,
+    		     	        # then update the bookkeeping variable
+                            if d < minDist[0]:
+                                minDist = (d, k)
+                        print('Color %i %i: %s' % (i,count,self.colorNames[minDist[1]]))
 
-    			    # if the distance is smaller than the current distance,
-    		     	# then update the bookkeeping variable
-                    if d < minDist[0]:
-                        minDist = (d, k)
-                print(self.colorNames[minDist[1]])
+            else:
+                self.cluster.append(None)
+
+    def get_shape(self):
+        for i in range(0, len(self.cluster)):
+            if self.cluster[i] is not None:
+                peri = cv.arcLength(self.c[i], True)
+                approx = cv.approxPolyDP(self.c[i], 0.02 * peri, True)
+                #print('Shape %i: %i' % (i,len(approx)))
 
     def classify(self, show=False):
 
-        self.crop_ROIs(show)
+        #print("crops: %i" % len(self.img_crop))
         self.get_mask(show)
         self.get_white_back(show)
         self.color_cluster(show)
+        self.get_shape()
 
     def clear_lists(self):
 
@@ -201,25 +270,41 @@ class AutonomousClassification():
         self.blur_crop.clear()
         self.canny_crop.clear()
         self.flood_crop.clear()
+        self.c.clear()
         self.big_contour.clear()
         self.white_back.clear()
         self.cluster.clear()
-        #self.centers.clear()
 
-detector = AutonomousDetection()
-classify = AutonomousClassification()
-img = cv.imread('../comp_target_images/target8.jpg')
-keypoints = detector.detect(img,1)
-
-classify.load_new_image(img, detector.resized_image.shape, keypoints)
-classify.classify(1)
-cv.waitKey(0)
-cv.destroyAllWindows()
-
-img = cv.imread('../comp_target_images/target7.jpg')
-keypoints = detector.detect(img,1)
-
-classify.load_new_image(img, detector.resized_image.shape, keypoints)
-classify.classify(1)
-cv.waitKey(0)
-cv.destroyAllWindows()
+#Possible Fail Cases:
+#   No countour found in the edge detector of this class
+#   No two obviously different colors found (k_means centers are too close together)
+#   No possible shape detected
+#   No letter detected
+#
+# detector = AutonomousDetection()
+# classify = AutonomousClassification()
+# img_crops = []
+#
+# count = 0
+# for i in range(0,1):
+#     img_crops.clear()
+#     img = cv.imread('../test_target_images/field_targets2.jpg')
+#     #img = cv.imread('../snow_target_images/snow%i.jpg' % (i+1))
+#     #img = cv.imread('../flight_pics/test%i.jpg' % (i))
+#     #img = cv.imread('../comp_target_images/target1.jpg')
+#     #img = cv.imread('../comp_no_target_images/field6.jpg')
+#     start = time.time()
+#     img_crops = detector.detect(img,1)
+#     print('Detector Time: %f' % (time.time()-start))
+#
+#     print("Keyponts length: %i" % (len(img_crops)))
+#     # cv.waitKey(0)
+#     # cv.destroyAllWindows()
+#
+#     if len(img_crops):
+#         start = time.time()
+#         classify.load_new_crops(img_crops)
+#         classify.classify(1)
+#         print('Classify Time: %f' % (time.time()-start))
+#         cv.waitKey(0)
+#         cv.destroyAllWindows()
--- autonomous/src/autonomous_detection.py
+++ autonomous/src/autonomous_detection.py
@@ -1,9 +1,5 @@
 import cv2 as cv
 import numpy as np
-from sklearn.cluster import MiniBatchKMeans
-from matplotlib import pyplot as plt
-from collections import OrderedDict
-from scipy.spatial import distance as dist
 import imutils
 import time
 
@@ -14,55 +10,113 @@ class AutonomousDetection():
 
         #set the parameters for blob detection
         params = cv.SimpleBlobDetector_Params()
-        params.minThreshold = 0;
-        params.maxThreshold = 200;
+        params.minThreshold = 0
+        params.maxThreshold = 200
         #filter by Area
+        params.filterByColor = False
         params.filterByArea = True
-        params.minArea = 10
-        params.maxArea = 200
+        params.minArea = 50#200
+        params.maxArea = 1000#2500
         #filter by Circularity
         params.filterByCircularity = False
         params.minCircularity = 0.7
         #filter by Convexity
-        params.filterByConvexity = False
-        params.minConvexity = 0.0
+        params.filterByConvexity = True
+        params.minConvexity = 0.5
         #filter by Inertia
-        params.filterByInertia = False
-        params.minInertiaRatio = 0.3
+        params.filterByInertia = True
+        params.minInertiaRatio = 0.1
+        params.maxInertiaRatio = 1.0
 
         #create the blob detector and detect significant blobs
         self.blob_detector = cv.SimpleBlobDetector_create(params)
 
+        self.img_crop = []
 
     def detect(self, img, show=0):
         self.original_image = img
-        self.resized_image = imutils.resize(img, width=600)
+        self.resized_image = imutils.resize(img, width=600) #bassler images
+        #self.resized_image = imutils.resize(img, width=1200) #sony images
+
+        self.y_orig, self.x_orig, _ = img.shape
+        self.y_resize, self.x_resize, _ = self.resized_image.shape
+
+        self.img_crop.clear()
 
         self.preprocess()
-        self.detectROI()
+        self.detect_ROIs(500)
+        self.crop_ROIs()
+
 
         if show:
             self.keypoints_image = cv.drawKeypoints(self.resized_image, self.keypoints,
                 np.array([]), (0,0,255), cv.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+            cv.imshow('Resized', self.resized_image)
+            cv.imshow('Preprocess', self.preprocessed)
+            cv.imshow('Canny', self.canny)
+            cv.imshow('Flood', self.flood)
             cv.imshow('Detection', self.keypoints_image)
-            cv.waitKey(0)
-            cv.destroyAllWindows()
+            for i in range(len(self.img_crop)):
+                cv.imshow('Cropped Image %i' % (i), self.img_crop[i])
+            # cv.waitKey(0)
+            # cv.destroyAllWindows()
 
-        return self.keypoints
+        return self.img_crop
 
     def preprocess(self):
         img_blur = cv.GaussianBlur(self.resized_image, (5, 5), 0)
-        self.preprocessed = cv.pyrMeanShiftFiltering(img_blur, 10, 10, 3) #bassler images
-        #self.preprocessed = cv.pyrMeanShiftFiltering(img_blur, 30, 30, 3) #sony images
+        #self.preprocessed = cv.pyrMeanShiftFiltering(self.resized_image, 10, 10, 3) #bassler images
+        self.preprocessed = cv.pyrMeanShiftFiltering(img_blur, 30, 30, 3) #sony images
+        #self.preprocessed = cv.bilateralFilter(img_blur, 10, 150, 150) #faster bilateral filter
 
-    def detectROI(self, edge_limit=500):
+    def detect_ROIs(self, edge_limit=500):
         self.canny = cv.Canny(self.preprocessed, 10, edge_limit)
-        self.keypoints = self.blob_detector.detect(self.canny)
+        self.canny = cv.dilate(self.canny, None, iterations=1)
+        self.canny = cv.erode(self.canny, None, iterations=1)
+        #self.floodfill()
+        self.flood = self.canny
+        self.keypoints = self.blob_detector.detect(self.flood)
+        self.reduce_keypoints()
 
+    def floodfill(self):
+        h, w = self.canny.shape[:2]
+        self.canny[0,:] = 0
+        self.canny[h-1,:] = 0
+        self.canny[:,0] = 0
+        self.canny[:,w-1] = 0
+        mask = np.zeros((h+2, w+2), np.uint8)
+        edges = self.canny.copy()
+        cv.floodFill(edges, mask, (0,0), 255)
+        edges = cv.bitwise_not(edges)
+        self.flood = self.canny | edges
 
-# detector = AutonomousDetection()
-# img = cv.imread('../comp_target_images/target7.jpg')
-# detector.detect(img,1)
-#
-# img = cv.imread('../comp_target_images/target8.jpg')
-# detector.detect(img,1)
+    #check to see if keypoints are too close to each other
+    def reduce_keypoints(self):
+        to_delete = []
+        for i in range(len(self.keypoints)):
+            if i not in to_delete:
+                for j in range(len(self.keypoints)):
+                    if i != j and j not in to_delete:
+                        dist = ((self.keypoints[j].pt[0]-self.keypoints[i].pt[0])**2+(self.keypoints[j].pt[1]-self.keypoints[i].pt[1])**2)**(1/2)
+                        if dist < 100:#100
+                            to_delete.append(j)
+        for n in to_delete:
+            self.keypoints[n] = None
+
+    def crop_ROIs(self):
+
+        for i in range(len(self.keypoints)):
+
+            if self.keypoints[i] is not None:
+                #find the keypoint in the resized picture
+                x = int(self.keypoints[i].pt[0])
+                y = int(self.keypoints[i].pt[1])
+
+                #find the keypoint in the original picture
+                x_new = int(self.x_orig*x/self.x_resize)
+                y_new = int(self.y_orig*y/self.y_resize)
+
+                #crop the potential target from the orignal image
+                #crop = img[y_new-30:y_new+40, x_new-30:x_new+40]
+                crop = self.original_image[max(1,y_new-90):min(y_new+120,self.y_orig), max(1,x_new-90):min(x_new+120,self.x_orig)] #sony image
+                self.img_crop.append(crop)
