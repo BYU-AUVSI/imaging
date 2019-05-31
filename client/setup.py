1cd5269c146faa96d089202f7559c23edd141699
len0rd <tylerm15@gmail.com>
2019-05-31 15:29:14 -0600

    removed dji image folder

--- client/setup.py
+++ client/setup.py
@@ -32,5 +32,8 @@ setuptools.setup(
      classifiers=[
          "Programming Language :: Python :: 3",
          "Operating System :: OS Independent",
+         "Programming Language :: Python :: 3.5",
+         "Programming Language :: Python :: 3.6",
+         "Programming Language :: Python :: 3.7",
      ],
  )
